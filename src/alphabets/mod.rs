80b41c312b42d8477fbab4b811342c4ec64dcfb0
Michizev <41794612+Michizev@users.noreply.github.com>
2022-10-29 11:48:26 +0200

    Adding CSV based code to the codebase (#2)
    * Minimum working example of the concept
    
    * Adding a csv based dynamic version

--- src/alphabets/mod.rs
+++ src/alphabets/mod.rs
@@ -1,11 +1,41 @@
 // Adding the reference to the class here
 pub mod korean;
 
-use std::collections::HashMap;
 
+use std::{collections::HashMap, fs::File, io::BufReader};
+use serde::{Deserialize};
+use std::error::Error;
  
 /// Abstraction of the Alphabet as a Hashmap of a single character to a transcription. 
 pub trait Alphabet<'a>
 {
     fn alphabet(&self) -> &HashMap<char, &'a str>;
 }
+
+#[derive(Debug, Deserialize)]
+pub struct LetterTranscription
+{
+    letter: char,
+    transcript: String,
+}
+
+
+pub fn parse_from_csv(filename:&str) -> Result<HashMap<char, String>, Box<dyn Error>>{
+    let f = File::open(filename)?;
+    let reader = BufReader::new(f);
+    let mut rdr = csv::Reader::from_reader(reader);
+
+    let mut map = HashMap::new();
+
+    for (_i, result) in rdr.deserialize().enumerate() {
+        // Notice that we need to provide a type hint for automatic
+        // deserialization.
+        let record: LetterTranscription = result?;
+        //println!("{} {:?}", _i, record);
+
+        map.insert(record.letter, record.transcript);
+    }
+    
+    Ok(map)
+}
+
