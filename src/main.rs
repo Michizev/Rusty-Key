32de81cc06143e7ecf64781dd4a92d84bbfa7dd9
Michizev <41794612+Michizev@users.noreply.github.com>
2022-10-29 11:43:57 +0200

    Adding a csv based dynamic version

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 
 mod alphabets;
-use alphabets::Alphabet;
+//use alphabets::Alphabet;
 
 use console::style;
 use console::Style;
@@ -15,12 +15,11 @@ fn main() {
     
     let mut state = GameState {score: 0};
 
+    let random_data = alphabets::parse_from_csv("data/korean.csv").unwrap();
+    // The old static data
+    let _korean_data = alphabets::korean::KoreanData::new();
 
-    //println!("{:?}", korean_simple_vocals);
-
-    let korean_data = alphabets::korean::KoreanData::new();
-
-    for (key, value) in korean_data.alphabet()
+    for (key, value) in random_data
     {
         println!("{}", style(key).cyan());
         //term.write_line("{}", key).unwrap();
@@ -28,7 +27,7 @@ fn main() {
         let _ = std::io::stdin().read_line(&mut line).unwrap();
         
         let mut style = Style::new();
-        if line.trim().eq(*value)
+        if line.trim().eq(&value)
         {
             state.score += 1;
             style = style.green();
